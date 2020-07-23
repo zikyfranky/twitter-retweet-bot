1740510934f6742b02e18167b4ed4c1dc67c23cb
Isaac Frank <masturisaacfrank@gmail.com>
2020-07-23 04:42:16 +0100

    Pushed

---
+++ README.md
@@ -0,0 +1,15 @@
+# The #100DaysOfCode Twitter bot 🤖
+
+[@CodeRelatedBot](https://twitter.com/CodeRelatedBot) tries to motivate and celebrates you as you are learning how to code. Once your tweet has any of these hashtags `#100DaysOfCode`, `#codeNewbie`, `#WomenWhoCode`, `#javascript`, `#python`, This bot helps you to reach more persons by liking, retweeting and maybe follow you.
+
+In a single line:
+I am a Twitter bot that motivates people participating in the #100DaysOfCode challenge.
+
+Contributions are welcome!
+
+## SETUP
+
+- [Create a new app](https://apps.twitter.com/app/new) on twitter and generate your API keys
+- Replace the API keys in `secrets.py` with yours
+- Run `pip install -r requirements.txt`
+- Run `python main.py`
---
+++ main.py
@@ -0,0 +1,58 @@
+from tweepy import API, OAuthHandler, TweepError, Stream, StreamListener
+from secrets import *
+import random
+import os
+
+
+auth = OAuthHandler(
+    consumer_key=consumer_key, consumer_secret=consumer_secret)
+auth.set_access_token(key=access_token_key, secret=access_token_secret)
+
+api = API(auth, wait_on_rate_limit=True)
+
+watch_list = ['#100DaysOfCode', '#codeNewbie',
+              '#WomenWhoCode', '#javascript']
+reply_list = ['#100DaysOfCode', '#codeNewbie']
+me = api.me().screen_name
+
+with open(os.path.dirname(__file__)+'/replies.txt', 'r') as f:
+    replies = f.readlines()
+
+
+class Listener(StreamListener):
+
+    def on_error(self, status_code):
+        print('error', status_code)
+
+    def on_status(self, status):
+        data = status._json
+        random.shuffle(replies)
+        index = random.randint(0, len(replies)-1)
+        reply = replies[index].replace('###', data['user']['screen_name'])
+        if me == data['user']['screen_name']:
+            print("Won't retweet own tweet")
+        else:
+            if data['in_reply_to_status_id'] == None:
+                if 1 in [1 if i in data['text'] else 0 for i in watch_list]:
+
+                    try:
+                        # Only reply if any of the hashtags in @reply_list is present
+                        if 1 in [1 if i in data['text'] else 0 for i in reply_list]:
+                            api.update_status(reply, data['id'])
+                        # Like the tweet
+                        api.create_favorite(data['id'])
+                        # Follow the Poster
+                        api.create_friendship(data['user']['screen_name'])
+                        # Finally retweet
+                        api.retweet(data['id'])
+                    except TweepError:
+                        print('Tweet already retweeted')
+                else:
+                    print("It's just a useless retweet")
+            else:
+                print('Just a Commnent')
+
+
+mylistner = Listener()
+stream = Stream(auth=api.auth, listener=mylistner)
+stream.filter(track=watch_list)
